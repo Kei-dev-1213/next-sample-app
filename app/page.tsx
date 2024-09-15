b063c3c548b4ff2a505b6ec2e5df682d492b2f64
Kei-dev-1213 <zz2003live@gmail.com>
2024-09-15 13:26:11 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  const key = process.env.SAMPLE_SECRET_KEY;
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <p>環境変数の値：{key}</p>
+    </>
+  );
 }
